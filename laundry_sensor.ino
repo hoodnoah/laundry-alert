f9e9249af512f6ace2a0ffd785db3c2133990c80
Noah Hood <hood.noah@icloud.com>
2024-07-31 11:50:50 -0400

    switch from piezo sensor to built-in accelerometer

--- laundry_sensor.ino
+++ laundry_sensor.ino
@@ -1,46 +1,47 @@
+#include <Arduino_LSM6DS3.h>
+
 const int BAUD_RATE = 9600;
-const int PIEZO_1 = A0;
-const int PIEZO_2 = A3;
-const float ADC_WIDTH = 4095.0;
-const float REFERENCE_VOLTAGE = 3.3;
 const int POLLING_INTERVAL = 1; // polling interval in ms
 
 void setup()
 {
-  // set pins as input
-  pinMode(PIEZO_1, INPUT);
-  pinMode(PIEZO_2, INPUT);
-
   Serial.begin(BAUD_RATE);
+  while (!Serial)
+    ; // wait for serial to be ready
+
+  if (!IMU.begin())
+  {
+    Serial.println("Failed to initialize IMU, halting.");
+    while (1)
+      ; // indefinite pause
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's:");
+  Serial.println("(x, y, z)");
 }
 
-// converts an analog reading to a voltage
-float analogToVoltage(int analog)
+void printAcceleration(float x, float y, float z)
 {
-  return analog / ADC_WIDTH * REFERENCE_VOLTAGE;
-}
-
-// prints readings in the format:
-// Pin 1: 0.00, Pin 2: 0,00\n
-void printReadings(float pin1Reading, float pin2Reading)
-{
-  Serial.print("Pin 1: ");
-  Serial.print(pin1Reading);
+  Serial.print("(");
+  Serial.print(x);
   Serial.print(", ");
-  Serial.print("Pin 2: ");
-  Serial.print(pin2Reading);
-  Serial.print("\n");
+  Serial.print(y);
+  Serial.print(", ");
+  Serial.print(z);
+  Serial.print(")\n");
 }
 
 void loop()
 {
-  int pin1Reading = analogRead(PIEZO_1);
-  int pin2Reading = analogRead(PIEZO_2);
+  float x, y, z;
 
-  float pin1Voltage = analogToVoltage(pin1Reading);
-  float pin2Voltage = analogToVoltage(pin2Reading);
-
-  printReadings(pin1Voltage, pin2Voltage);
-
-  delay(POLLING_INTERVAL);
+  if (IMU.accelerationAvailable())
+  {
+    IMU.readAcceleration(x, y, z);
+    printAcceleration(x, y, z);
+  }
 }
